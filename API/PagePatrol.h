dfe65af1c0e48749384dc3c327d635a52d09aa5f
debian <debian@debian.debian.org>
2024-12-11 14:52:56 +0100

    improved API with index

--- API/PagePatrol.h
+++ API/PagePatrol.h
@@ -13,7 +13,7 @@ int pid_va_map_fd;
 int idx;
 
 inline int init_page_patrol () {
-	idx = 0;
+	idx = 1; // we skip zero as that is where we write the index
 
 	pid_va.flags = 0; // this field is set to the api
 	pid_va.VA = 0; // this field is set to the api
@@ -26,6 +26,13 @@ inline int init_page_patrol () {
 		return -1;
 	}
 
+	// write the index to map
+	int zero = 0;
+	if (bpf_map_update_elem(pid_va_map_fd, &zero, &idx, BPF_ANY) < 0) {
+		printf("Failed to update PID VA map for index %d)\n", idx);
+		return -1;
+	}
+
 	return 0;
 }
 
@@ -33,13 +40,23 @@ inline int mark_va_for_eviction (void * va) {
 	pid_va.VA = (unsigned long)(va);
 	pid_va.flags = 0;	// Eviction
 	
+	// write the value
 	if (bpf_map_update_elem(pid_va_map_fd, &idx, &pid_va, BPF_ANY) < 0) {
 		printf("Failed to update PID VA map for Evicting VA(0x%lx)\n", pid_va.VA);
 		return -1;
 	}
 
 	idx++;
-	idx %= MAX_PID_VA_ENTRIES;    // Max size of PID_VA map
+	if (idx == MAX_PID_VA_ENTRIES) {
+		idx = 1; // we must skip zero as that is where we place the index
+	}
+
+	// write the new index
+	int zero = 0;
+	if (bpf_map_update_elem(pid_va_map_fd, &zero, &idx, BPF_ANY) < 0) {
+		printf("Failed to update index %d)\n", idx);
+		return -1;
+	}
 
 	return 0;
 }
@@ -54,7 +71,16 @@ inline int pin_va (void * va) {
 	}
 
 	idx++;
-	idx %= MAX_PID_VA_ENTRIES;    // Max size of PID_VA map
+	if (idx == MAX_PID_VA_ENTRIES) {
+		idx = 1; // we must skip zero as that is where we place the index
+	}
+
+	// write the new index
+	int zero = 0;
+	if (bpf_map_update_elem(pid_va_map_fd, &zero, &idx, BPF_ANY) < 0) {
+		printf("Failed to update index %d)\n", idx);
+		return -1;
+	}
 
 	return 0;
 }
@@ -69,7 +95,16 @@ inline int unpin_va (void * va) {
 	}
 
 	idx++;
-	idx %= MAX_PID_VA_ENTRIES;    // Max size of PID_VA map
+	if (idx == MAX_PID_VA_ENTRIES) {
+		idx = 1; // we must skip zero as that is where we place the index
+	}
+
+	// write the new index
+	int zero = 0;
+	if (bpf_map_update_elem(pid_va_map_fd, &zero, &idx, BPF_ANY) < 0) {
+		printf("Failed to update index %d)\n", idx);
+		return -1;
+	}
 
 	return 0;
 }
@@ -84,7 +119,17 @@ inline int access_va (void * va) {
 	}
 
 	idx++;
-	idx %= MAX_PID_VA_ENTRIES;    // Max size of PID_VA map
+	if (idx == MAX_PID_VA_ENTRIES) {
+		idx = 1; // we must skip zero as that is where we place the index
+	}
+
+	// write the new index
+	int zero = 0;
+	if (bpf_map_update_elem(pid_va_map_fd, &zero, &idx, BPF_ANY) < 0) {
+		printf("Failed to update index %d)\n", idx);
+		return -1;
+	}
+
 
 	return 0;
 }
